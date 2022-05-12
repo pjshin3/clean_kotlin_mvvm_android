195cc6ec3ba00c31197a05ad077cd718cc1738c4
Pil Je Shin <tlsvlfwp1@naver.com>
2022-05-13 06:30:02 +0900

    dimens add

---
+++ app/src/main/java/com/example/clean_kotlin_mvvm/view/Dimens.kt
@@ -0,0 +1,17 @@
+package com.example.clean_kotlin_mvvm.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.Dp
+import com.example.clean_kotlin_mvvm.R
+
+object Dimens {
+    val PaddingSmall : Dp
+    @Composable get() = dimensionResource(id = R.dimen.padding_small)
+    val PaddingNormal : Dp
+    @Composable get() = dimensionResource(id = R.dimen.padding_normal)
+    val PaddingLarge : Dp
+    @Composable get() = dimensionResource(id = R.dimen.padding_large)
+    val PaddingXLarge : Dp
+    @Composable get() = dimensionResource(id = R.dimen.padding_xlarge)
+}

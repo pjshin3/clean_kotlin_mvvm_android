97e7fdffe82170543955375ace44f528288b7800
Pil Je Shin <tlsvlfwp1@naver.com>
2022-05-15 19:06:55 +0900

    compose modifier sample code add

--- app/src/main/java/com/example/clean_kotlin_mvvm/view/MainView.kt
+++ app/src/main/java/com/example/clean_kotlin_mvvm/view/MainView.kt
@@ -1,12 +1,26 @@
 package com.example.clean_kotlin_mvvm.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.clean_kotlin_mvvm.R
 
 
 @Preview
 @Composable
 fun HelloWorldText(){
-    Text("Hello World")
+    Row(modifier = Modifier.run {
+        fillMaxSize()
+        background(Color.Green)
+    }) {
+        Text(modifier = Modifier.run {
+            padding(Dimens.PaddingXLarge)
+        }, text = "Hello World")
+    }
 }

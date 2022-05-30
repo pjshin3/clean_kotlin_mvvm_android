e74060e7fc9225a338c3e5432bda4d11544ea427
Pil Je Shin <tlsvlfwp1@naver.com>
2022-05-31 06:31:10 +0900

    compose column add

--- app/src/main/java/com/example/clean_kotlin_mvvm/compose/MainView.kt
+++ app/src/main/java/com/example/clean_kotlin_mvvm/compose/MainView.kt
@@ -1,12 +1,14 @@
 package com.example.clean_kotlin_mvvm.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -19,27 +21,27 @@ import androidx.navigation.compose.rememberNavController
 @Preview
 @Composable
 fun MainScreen(){
-    val controller = rememberNavController()
-
-
     var isVisible by remember { mutableStateOf(true) }
     val focusRequest = remember { FocusRequester() }
         Row(modifier = Modifier.run {
             fillMaxSize()
             background(Color.Green)
         }) {
+            Column(modifier = Modifier.run {
+                align(Alignment.CenterVertically)
+            }) {
+                ClickableText(modifier = Modifier.run {
+                    padding(Dimens.PaddingXLarge)
+                }, text = AnnotatedString( "click me!!"),
+                    onClick = { offset ->
+                        isVisible = !isVisible
+                    })
 
-            ClickableText(modifier = Modifier.run {
-                padding(Dimens.PaddingXLarge)
-            }, text = AnnotatedString( "click me!!"),
-                onClick = { offset ->
-                    isVisible = !isVisible
-                })
 
-
-            if (isVisible){
-                Text(modifier = Modifier.focusRequester(focusRequest),
-                    text = if (isVisible) "VISIBLE" else "INVISIBLE")
+                if (isVisible){
+                    Text(modifier = Modifier.focusRequester(focusRequest),
+                        text = if (isVisible) "VISIBLE" else "INVISIBLE")
+                }
             }
         }
 
